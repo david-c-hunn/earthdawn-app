c7f8e7433bc196a7c2812c4ea2d2bc93702d244b
Dave <david.c.hunn@gmail.com>
2016-10-06 21:00:25 -0700

    removed unused route from app config

--- app/app.config.js
+++ app/app.config.js
@@ -7,7 +7,6 @@ angular.module('earthdawnApp').config([
 
     $routeProvider
       .when('/talents', {template: '<talent-list></talent-list>'})
-      .when('/talents/edit', {template: '<talent-editor></talent-editor>'})
       .when('/talents/edit/:talentId', {template: '<talent-editor></talent-editor>'})
       .when('/spells', {template: '<spell-list></spell-list>'})
       .otherwise('/talents');
